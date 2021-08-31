06e491b412a33468ab4a3ee8018f1c6f41606f35
Khansa Syafika <84486407+khnssyfk@users.noreply.github.com>
2021-08-31 07:43:43 +0700

    Add files via upload
    First commit

---
+++ dropdown.js
@@ -0,0 +1,8 @@
+function show_hide() {
+    var click = document.getElementsByClassName("drop-content");
+    if(click[0].style.display ==="none") {
+       click[0].style.display ="block";
+    } else {
+       click[0].style.display ="none";
+    } 
+ }
